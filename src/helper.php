24fb1203ab97f75aa9254c24f81853e58afb6cd7
kwan <303198069@qq.com>
2018-05-30 01:54:06 +0800

    init

---
+++ src/helper.php
@@ -0,0 +1,28 @@
+<?php
+
+use think\facade\Lang;
+
+if (!function_exists('__')) {
+
+    /**
+     * 获取语言变量值
+     * @param string $name 语言变量名
+     * @param array $vars 动态变量值
+     * @param string $lang 语言
+     * @return mixed
+     */
+    function __($name, $vars = [], $lang = '')
+    {
+        if (is_numeric($name) || !$name) {
+            return $name;
+        }
+        if (!is_array($vars)) {
+            $vars = func_get_args();
+            array_shift($vars);
+            $lang = '';
+        }
+
+        return Lang::get($name, $vars, $lang);
+    }
+
+}
